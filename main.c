f39514d50bacdbd131c0bd4ed3b3000a2e3d501d
Igor Enrick de Carvalho <igorenrickc@gmail.com>
2022-06-29 15:17:50 -0300

    Função 8 botões e 8 LEDs

--- main.c
+++ main.c
@@ -50,12 +50,74 @@ int main(void) {
 		delay_ms(1000);
 		mcp23S17_escreve_pino(MCP_PIN_B0, LOW);
 		delay_ms(1000);
-		if (mcp23S17_le_pino(MCP_PIN_A0) == HIGH) {
-			printf("Botao em VCC\n");
-		} else {
-			printf("Botao em GND\n");
-		}
+		botao_led();
 	}
 	
 	return 0;
+}
+
+void botao_led() {
+	if (mcp23S17_le_pino(MCP_PIN_A0) == HIGH) {
+		printf("Botao A0 em VCC\n");
+		mcp23S17_escreve_pino(MCP_PIN_B0, HIGH);
+	} else {
+		printf("Botao A0 em GND\n");
+		mcp23S17_escreve_pino(MCP_PIN_B0, LOW);
+	}
+
+	if (mcp23S17_le_pino(MCP_PIN_A1) == HIGH) {
+		printf("Botao A1 em VCC\n");
+		mcp23S17_escreve_pino(MCP_PIN_B1, HIGH);
+	} else {
+		printf("Botao A1 em GND\n");
+		mcp23S17_escreve_pino(MCP_PIN_B1, LOW);
+	}
+
+	if (mcp23S17_le_pino(MCP_PIN_A2) == HIGH) {
+		printf("Botao A2 em VCC\n");
+		mcp23S17_escreve_pino(MCP_PIN_B2, HIGH);
+	} else {
+		printf("Botao A2 em GND\n");
+		mcp23S17_escreve_pino(MCP_PIN_B2, LOW);
+	}
+
+	if (mcp23S17_le_pino(MCP_PIN_A3) == HIGH) {
+		printf("Botao A3 em VCC\n");
+		mcp23S17_escreve_pino(MCP_PIN_B3, HIGH);
+	} else {
+		printf("Botao A3 em GND\n");
+		mcp23S17_escreve_pino(MCP_PIN_B3, LOW);
+	}
+
+	if (mcp23S17_le_pino(MCP_PIN_A4) == HIGH) {
+		printf("Botao A4 em VCC\n");
+		mcp23S17_escreve_pino(MCP_PIN_B4, HIGH);
+	} else {
+		printf("Botao A4 em GND\n");
+		mcp23S17_escreve_pino(MCP_PIN_B4, LOW);
+	}
+
+	if (mcp23S17_le_pino(MCP_PIN_A5) == HIGH) {
+		printf("Botao A5 em VCC\n");
+		mcp23S17_escreve_pino(MCP_PIN_B5, HIGH);
+	} else {
+		printf("Botao A5 em GND\n");
+		mcp23S17_escreve_pino(MCP_PIN_B5, LOW);
+	}
+
+	if (mcp23S17_le_pino(MCP_PIN_A6) == HIGH) {
+		printf("Botao A6 em VCC\n");
+		mcp23S17_escreve_pino(MCP_PIN_B6, HIGH);
+	} else {
+		printf("Botao A6 em GND\n");
+		mcp23S17_escreve_pino(MCP_PIN_B6, LOW);
+	}
+
+	if (mcp23S17_le_pino(MCP_PIN_A7) == HIGH) {
+		printf("Botao A7 em VCC\n");
+		mcp23S17_escreve_pino(MCP_PIN_B7, HIGH);
+	} else {
+		printf("Botao A7 em GND\n");
+		mcp23S17_escreve_pino(MCP_PIN_B7, LOW);
+	}
 }
